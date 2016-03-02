7fd4c9dfcc063dbdb2ea074365fff7bdd7227691
zhulinpinyu <mulixiang199016@gmail.com>
2016-03-02 17:03:00 +0800

    update

--- app/controllers/unauthenticated_controller.rb
+++ app/controllers/unauthenticated_controller.rb
@@ -8,5 +8,8 @@ class UnauthenticatedController < ActionController::Metal
     self.status = :unauthorized
     self.content_type = "application/json"
     self.response_body = {errors: ["Unauthorized Request"]}.to_json
+    self.headers["Access-Control-Allow-Origin"]  = "*"
+    self.headers["Access-Control-Allow-Methods"] = %w{GET POST PUT OPTIONS DELETE}.join(',')
+    self.headers["Access-Control-Allow-Headers"] = %w{Content-Type Accept X-User-Email X-Auth-Token}.join(',')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   resource :sessions, only: [:create, :destroy]
   resources :customers, only: [:index,:show,:create,:update,:destroy]
+
+  match "/*path", to: proc {
+    [
+      204,
+      {
+        "Content-type" => "text/plain",
+        "Access-Control-Allow-Origin" => "*",
+        "Access-Control-Allow-Methods" => %w{GET POST PUT OPTIONS DELETE}.join(','),
+        "Access-Control-Allow-Headers" => %w{Content-Type Accept X-User-Email X-Auth-Token}.join(',')
+      },
+      []
+    ]
+  }, via: [:options, :head]
 end
